e2ac249abe4abed9d717e275060b7b4756ff08c1
Rocky Das <rockydasgfx@gmail.com>
2021-02-07 23:07:45 +0600

    I can't Do this

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
+.then(res => res.json())
+.then(data => displayMeals(data));
+
+const displayMeals = meals => {
+    for (let i = 0; i < meals.length; i++) {
+        const meal = meals[i];
+        console.log(meal.idMeal);
+        
+    }
+}
